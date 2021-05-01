730363a71fd33d724cc88931bf62a5d350cb9b3f
Andreas Kroll <andreas.kroll@yahoo.de>
2021-05-01 08:03:53 +0200

    Create tl_member.php

---
+++ src/Resources/contao/languages/en/tl_member.php
@@ -0,0 +1,4 @@
+<?php
+$GLOBALS['TL_LANG']['tl_member']['doccheck_legend'] = 'DocCheck-Login';
+$GLOBALS['TL_LANG']['tl_member']['doccheck_user'] = array('Is a member of DocCheck','This member can view protected content.');
+$GLOBALS['TL_LANG']['tl_member']['doccheck_redirect'] = array('Redirect page','Choose a redirect page, to define which site is visible after login.');
